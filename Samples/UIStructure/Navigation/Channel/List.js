cdfb592b38a3954f3922c2f4530033717cfc2456
edA-qa on MBP <edA-qa@disemia.com>
2017-06-23 11:49:59 +0200

    delete outdated navigation

--- Samples/UIStructure/Navigation/Channel/List.js
+++
@@ -1,8 +0,0 @@
-var Observable = require("FuseJS/Observable")
-var channels = require("App/channels.js")
-
-exports.view = function(e) {
-	router.push("home", {}, "channels", {}, "view", e.data.name )
-}
-
-exports.channels = channels.channels
